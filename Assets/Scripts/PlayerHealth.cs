1362b77fb03e5d9f99649d5b0e0e0d2e7fde30cd
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2021-07-22 17:27:11 +0200

    Creating a Death Handler

--- Assets/Scripts/PlayerHealth.cs
+++ Assets/Scripts/PlayerHealth.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     [SerializeField] float hitPoints = 100f;
+    //DeathHandler deathHandler;
 
     // create a public method which reduces hitpoints by the amount of damage
+
+
 
     public void TakeDamage(int damage)
     {
@@ -14,6 +17,8 @@ public class PlayerHealth : MonoBehaviour
         Debug.Log("Enemy have now " + hitPoints + " health");
         if (hitPoints <= 0)
         {
+            //deathHandler.HandleDeath();
+            GetComponent<DeathHandler>().HandleDeath(); //we dont do variable of scrpit and above call because we would have to add getcomponent in start because of nullreferenceError in game - this is much faster way because we need only call 1 method
             Debug.Log("You dead ");
         }
     }
--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -7,8 +7,9 @@ public class SceneLoader : MonoBehaviour
 {
     public void RealoadLevel()
     {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex; // this variable + load scene is usefull if we have multiple scenes, otherwise just use SceneManager.LoadScene(0);
         SceneManager.LoadScene(currentSceneIndex);
+        Time.timeScale = 1;
     }
 
     public void QuitGame()
