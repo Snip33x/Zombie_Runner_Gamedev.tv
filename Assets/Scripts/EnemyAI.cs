c1598f20c744dca444083622b24aa2885f7ae4c1
Rafał Kulawiak <62555963+Snip33x@users.noreply.github.com>
2021-08-09 22:44:17 +0200

    Update EnemyAI.cs

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -65,7 +65,7 @@ public class EnemyAI : MonoBehaviour
     private void ChaseTarget()
     {
         GetComponent<Animator>().SetBool("attack", false);
-        //String reference to Enemy object - animator -parameters-move(created and named by me)d
+        //String reference to Enemy object - animator -parameters-move(created and named by me)dd
         GetComponent<Animator>().SetTrigger("move");
         navMeshAgent.SetDestination(target.position);
     }
